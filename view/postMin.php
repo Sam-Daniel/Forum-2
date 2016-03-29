9944bf08d7e72c5397205b7ce26112f04293944b
dev <dev@dev5.bioshazard.com>
2016-03-29 14:43:42 -0400

    'forgot these'

---
+++ view/postMin.php
@@ -0,0 +1,11 @@
+<div class="row">
+	<div class="col-2">
+		<img src="http://placehold.it/100x100" alt="" />
+	</div>
+	<div class="col-10">
+		<ul class="vlist">
+			<li><a href="<?=\lf\requestGet('ActionUrl');?>read/<?=$post['id'];?>/<?=urlencode($post['title']);?>"><?=$post['title'];?></a></li>
+			<li><?=$post['display_name'];?>, # views, # comments, Most recent by SOMEGUY SOMETIME</li>
+		</ul>
+	</div>
+</div>
---
+++ view/threadOp.php
@@ -0,0 +1,22 @@
+<div class="row">
+	<div class="col-2">
+		<ul class="vlist">
+			<li>
+				<?=$post['display_name'];?>
+			</li>
+			<li>
+				<img src="http://placehold.it/100x100" alt="" />
+			</li>
+		</ul>
+	</div>
+	<div class="col-10">
+		<ul class="vlist">
+			<!-- not needed for main page
+			 - <?=$post['date'];?> - <?=$post['category'];?> - Latest Post: asdf
+			 
+			<li><?=$post['content'];?></li>
+			 -->
+			<li><?=$post['title'];?></li>
+		</ul>
+	</div>
+</div>
